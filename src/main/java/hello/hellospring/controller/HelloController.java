d3a2ed9e52cf4d9dc017324322e2e58d17d99cd6
Derit7042 <junsu6752@gmail.com>
2024-02-15 16:44:22 +0900

    섹션 2 스프링 웹 개발 기초 - (3) API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,4 +19,32 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // parameter 의 name 에 spring 을 받으면 return "hello spring"; 으로 된다.
+    }
+
+    // 근데 만약 문자 이런 것이 아니라 뭔가 데이터를 내놓으라고 한다면? 이것 때문에 API 방식을 많이 쓴다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // hello 라는 문자가 아닌 객체를 넣었다. 이러면 어떻게 될까?
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
